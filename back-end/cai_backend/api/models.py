8e3a929107ea448c031ad713a9ee1893f94ee643
pavobravo11 <gustavoa1011@gmail.com>
2025-03-05 17:07:08 -0700

    Fixed syntax errors in models.py

--- back-end/cai_backend/api/models.py
+++ back-end/cai_backend/api/models.py
@@ -30,25 +30,6 @@ class DietaryRestriction(models.Model):
     user = models.ForeignKey(User, related_name="dietary_restrictions", on_delete=models.CASCADE)
     restriction = models.CharField(max_length=200, blank=True, help_text="Dietary restrictions, seperated by a comma (,)")
 
-class RegisteredUser(User):
-    """Information about logged in user, boolean provides admin privelages"""
-    first_name = models.CharField(max_length=50, blank=False, null=False)
-    last_name = models.CharField(max_length=50, blank=False, null=False)
-    username = models.CharField(max_length=50, blank=False, null=False)
-    email = models.EmailField(max_length=254, blank=False, null=False)
-    hashed_password = models.charField(max_length=254, blank=False, null=False)
-    is_admin = models.BooleanField(default=False)
-    saved_recipes = models.ManyToManyField(Recipe, related_name="saved_recipes", blank=True)
-    last_used_recipes = models.ManyToManyField(Recipe, related_name="last_used_recipes", blank=True)
-
-    def add_viewed_recipe(self, recipe):
-        """Adds new recipes up to 5, uses FIFO to drop the oldest viewed recipe"""
-        self.last_used_recipes.add(recipe)
-
-        if self.last_used_recipes.count() > 5:
-            first_viewed = self.last_viewed_recipes.all().order_by("id").first()
-            self.last_viewed_recipes.remove(first_viewed)
-
 class Recipe(models.Model):
     title = models.CharField(max_length=100, blank=False, null=False)
     estimated_time = models.CharField(
@@ -66,3 +47,22 @@ class Recipe(models.Model):
 class Ingredients(models.Model):
     recipe = models.ForeignKey(Recipe, related_name="ingredients", on_delete=models.CASCADE)
     ingredient = models.CharField(max_length=50, blank=False, null=False)
+
+class RegisteredUser(User):
+    """Information about logged in user, boolean provides admin privelages"""
+    first_name = models.CharField(max_length=50, blank=False, null=False)
+    last_name = models.CharField(max_length=50, blank=False, null=False)
+    username = models.CharField(max_length=50, blank=False, null=False)
+    email = models.EmailField(max_length=254, blank=False, null=False)
+    hashed_password = models.CharField(max_length=254, blank=False, null=False)
+    is_admin = models.BooleanField(default=False)
+    saved_recipes = models.ManyToManyField(Recipe, related_name="saved_recipes", blank=True)
+    last_used_recipes = models.ManyToManyField(Recipe, related_name="last_used_recipes", blank=True)
+
+    def add_viewed_recipe(self, recipe):
+        """Adds new recipes up to 5, uses FIFO to drop the oldest viewed recipe"""
+        self.last_used_recipes.add(recipe)
+
+        if self.last_used_recipes.count() > 5:
+            first_viewed = self.last_used_recipes.all().order_by("id").first()
+            self.last_used_recipes.remove(first_viewed)
