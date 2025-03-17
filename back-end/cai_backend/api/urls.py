013723df71a1990b3da62a93bbcd2d47f63cacb1
arush sanghal <arush.sanghal@ucalgary.ca>
2025-03-17 12:06:58 -0600

    Update Profile Connection

--- back-end/cai_backend/api/urls.py
+++ back-end/cai_backend/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import register_user, login_user, get_user_data
+from .views import register_user, login_user, get_user_data, update_user_profile
 
 urlpatterns = [
     path('register/', register_user, name='register'),
     path('login/', login_user, name='login'),
     path('user/', get_user_data, name='user-data'),
+    path('update-user/', update_user_profile, name='update-user'),
 ]
