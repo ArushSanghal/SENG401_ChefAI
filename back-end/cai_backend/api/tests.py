ac53150d97d0cd868bb279c27dc7543e1e4115c4
ArushSanghal <101238461+ArushSanghal@users.noreply.github.com>
2025-03-19 18:46:16 -0600

    Test cases added for successful login and profile updating

--- back-end/cai_backend/api/tests.py
+++ back-end/cai_backend/api/tests.py
@@ -1,10 +1,16 @@
 from django.test import TestCase
 from django.core.files.base import ContentFile
 from django.contrib.auth.hashers import make_password
-from .models import RegisteredUser, Recipe, Ingredients  # import your models
+from .models import RegisteredUser, Recipe, Ingredients, Token, SkillLevelChoices  # import your models
 from .views import Registered
+from .profile_manager import profileManager
+from authentication import Authenticator
+from recipe_manager import RecipeManager
 import json
 import os
+from datetime import datetime, timedelta
+from django.utils import timezone
+import uuid
 
 class SaveRecipeTestCase(TestCase):
     def setUp(self):
@@ -36,3 +42,80 @@ class SaveRecipeTestCase(TestCase):
         self.assertEqual(ingredients[1].ingredient, "onion")
 
 
+
+
+
+class UpdateProfileTest(TestCase):
+    def setUp(self):
+        # Create a mock registered user
+        self.user = RegisteredUser.objects.create(
+            first_name="John",
+            last_name="Doe",
+            username="jd1",
+            email="jd1@gmail.com",
+            hashed_password=make_password("testing1")
+        )
+        
+        self.token = Token.objects.create(
+            user=self.user,
+            token=str(uuid.uuid4()),
+            expires_at=timezone.now() + timedelta(minutes=10)
+        )
+
+        # Instantiate the Registered user class from your code
+        self.profile_manager = profileManager(self=self.user)
+
+    def test_update_profile(self):
+        new_data = {
+            "skill_level": SkillLevelChoices.INTERMEDIATE,
+            "dietary_restrictions": {"Beef"}
+        }
+            
+        response, status_code = self.profile_manager.update_profile(self.token.token, new_data)
+        
+        self.assertEqual (status_code, 200)
+        self.assertEqual(response,{"success": "Profile updated successfully"})
+        
+        
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.skill_level, SkillLevelChoices.INTERMEDIATE)
+        
+        dietary_restrictions = list(self.user.dietary_restrictions.value_list("restriction",  flat=True))
+        self.assertEqual(len(dietary_restrictions), 1)
+        self.assertEqual("Beef", dietary_restrictions)
+
+
+class LoginTest(TestCase):
+    def setUp(self):
+        # Create a mock registered user
+        self.user = RegisteredUser.objects.create(
+            first_name="John",
+            last_name="Doe",
+            username="jd2",
+            email="jd2@gmail.com",
+            hashed_password=make_password("testing1")
+        )
+
+    def test_successful_login(self):
+        authenticator = Authenticator(email_address="jd2@gmail.com", password="testing1")
+
+        response, status_code = authenticator.sign_in()
+
+        self.assertEqual(status_code, 200)
+        self.assertEqual(response["message"], f"Welcome back, {self.user.first_name}!")
+
+        token = Token.objects.filter(user=self.user).first()
+        self.assertIsNotNone(token)
+        self.assertTrue(token.is_valid())
+
+
+        self.assertEqual(response["user_id"], self.user.id)
+        self.assertEqual(response["first_name"], self.user.first_name)
+        self.assertEqual(response["last_name"], self.user.last_name)
+        self.assertEqual(response["username"], self.user.username)
+        self.assertEqual(response["email"], self.user.email)
+        self.assertEqual(response["skill_level"], self.user.skill_level)
+        self.assertEqual(response["dietary_restrictions"], list(self.user.dietary_restrictions.values_list("restriction", flat=True)))
+
+        self.assertIn("access", response)
+        self.assertIn("refresh", response)
