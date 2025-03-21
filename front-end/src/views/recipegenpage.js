bb1a65720c66b6a046516b6e2640d7c69d2e0ad5
Sarah10109 <62390879+Sarah10109@users.noreply.github.com>
2025-03-21 12:19:33 -0600

    Update recipegenpage.js

--- front-end/src/views/recipegenpage.js
+++ front-end/src/views/recipegenpage.js
@@ -2,7 +2,6 @@ import React, { Fragment, useState, useEffect } from 'react'
 
 import './recipegenpage.css'
 import '../components/food-form.css'
-// import '../components/navbar8'
 import NavbarGuest from '../components/navbarGuest';
 
 function Recipegenpage(){
@@ -51,7 +50,6 @@ function Recipegenpage(){
     <title>ChefAI</title>
     <meta property="og:title" content="ChefAI" />
 
-    {/* <Navbar8></Navbar8> */}
     <NavbarGuest></NavbarGuest>
 
     <img src="/images/spaghetti-with-vegetables-cooking-in-a-pan.png" alt="frying pan" className="image"></img>
